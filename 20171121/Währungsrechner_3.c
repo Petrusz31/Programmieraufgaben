fe18c04f72815f5969ce20c90a231485dce0412a
vb4 <33378779+vb4@users.noreply.github.com>
2017-11-21 12:00:22 +0100

    Test

--- 20171121/Währungsrechner_3.c
+++ 20171121/Währungsrechner_3.c
@@ -1,13 +1,27 @@
 #include "stdio.h"
 
+const double DLR_EUR = 0.85
+
+double umrechnung(double wert, int ursprung, int ende);
 
 int main() {
 
+int urspw, endw;
 
 printf("Waehlen sie die Ursprungswaehrung: \n[1]\n[2]\n[3]");
-
+scanf("%d", urspw);
 printf("Waehlen sie die Endwaehrung: \n[1]\n[2]\n[3]");
+scanf("%d", endw);
 
 
 
+
+}
+
+double umrechnung(double wert, int ursprung, int ende) {
+    double resultat;
+
+
+
+    return resultat;
 }
