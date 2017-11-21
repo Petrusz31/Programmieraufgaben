3a06b345bc73a50dda9ed6803ed06acc6dc8f860
vb4 <33378779+vb4@users.noreply.github.com>
2017-11-21 13:16:29 +0100

    test

--- 20171121/Währungsrechner_3.c
+++ 20171121/Währungsrechner_3.c
@@ -2,27 +2,38 @@
 
 const double USD_EUR = 0.85;
 
-double umrechnung(double wert, int ursprung, int ende);
+double umrechnung(double wert, int wahl);
 
 int main() {
-int urspw, endw;
+char urspw, endw;
+int wahl;
 double menge;
 
-printf("Waehlen sie die Ursprungswaehrung: \n[1] EUR\n[2] USD\n[3]");
-scanf("%d", &urspw);
-printf("Waehlen sie die Endwaehrung: \n[1] EUR\n[2] USD\n[3]");
-scanf("%d", &endw);
+printf("Waehlen sie die Ursprungswaehrung: \n[E] EUR\n[U] USD\n[3]");
+urspw = getchar();
+printf("Waehlen sie die Endwaehrung: \n[E] EUR\n[U] USD\n[3]");
+endw = getchar();
+if (urspw == endw) {
+    printf("Fehlerhafte Eingabe!");
+    return 0;
+}
 printf("Geben sie den zu rechnenden Geldwert ein: ");
 scanf("%lf", &menge);
+wahl = urspw - endw;
 
-
-printf("Resultat: %f", umrechung());
+printf("Resultat: %lf", umrechnung(menge, wahl));
 
 }
 
-double umrechnung(double wert, int ursprung, int ende) {
+double umrechnung(double wert, int wahl) {
     double resultat;
+    switch (wahl) {
+        case 'E' - 'U': resultat = wert*USD_EUR; return 0.1;
 
+        case 'U' - 'E': resultat = wert/USD_EUR; return 1.1;
+        default: return 2.2;
+
+    }
 
 
     return resultat;
