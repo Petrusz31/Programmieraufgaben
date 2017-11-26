2e871a22652ca7663c4784284b16e73c213713b6
vb4 <33378779+vb4@users.noreply.github.com>
2017-11-26 14:21:44 +0100

    Funktionen ausprobiert

---
+++ Test/Funktionen.c
@@ -0,0 +1,21 @@
+#include "stdio.h"
+
+int main() {
+int scanner;
+
+printf("%d", printf("Hello World!"));
+
+printf("\n");
+
+if(scanf("%d", &scanner)) {
+
+printf("%d", printf("Hello World!\n"));
+
+}
+else {
+printf("%d", printf("Nope!\n"));
+
+
+}
+
+}
