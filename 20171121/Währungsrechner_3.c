dfd3b144321ebafcdf748e4f74170145dcee8e48
vb4 <33378779+vb4@users.noreply.github.com>
2017-12-05 07:52:53 +0100

    Währungsrechner in nicht scheiße

--- 20171121/Währungsrechner_3.c
+++ 20171121/Währungsrechner_3.c
@@ -1,20 +1,26 @@
 #include "stdio.h"
+#include "stdlib.h" //Notwendig für Exit
 
 const double USD_EUR = 0.85;
+const double EUR_DM = 2;
+const double USD_DM = 0.85*2;
 
 double umrechnung(double wert, int wahl);
+void check(char wahl);
 
 int main() {
 char urspw, endw;
 int wahl;
 double menge;
 
-printf("Waehlen sie die Ursprungswaehrung: \n[E] EUR\n[U] USD\n[3] \n");
+printf("Waehlen sie die Ursprungswaehrung: \n[E] EUR\n[U] USD\n[D] DM\n");
 scanf("%c", &urspw);
 getchar();
-printf("Waehlen sie die Endwaehrung: \n[E] EUR\n[U] USD\n[3] \n");
+check(urspw);
+printf("Waehlen sie die Endwaehrung: \n[E] EUR\n[U] USD\n[D] DM\n");
 scanf("%c", &endw);
 getchar();
+check(endw);
 if (urspw == endw) {
     printf("Fehlerhafte Eingabe!");
     return 0;
@@ -24,20 +30,30 @@ scanf("%lf", &menge);
 getchar();
 wahl = urspw - endw;
 
-printf("Resultat: %lf", umrechnung(menge, wahl));
+printf("Resultat: %.2lf", umrechnung(menge, wahl));
 
 }
 
 double umrechnung(double wert, int wahl) {
-    double resultat;
-    switch (wahl) {
-        case 'E' - 'U': resultat = wert*USD_EUR; return 0.1;
+    switch (wahl) {     //Eigentlich muss man erst überprüfen, ob andere Zeichen als E oder U eingegeben wurden
+        case 'E' - 'U': return wert*USD_EUR;
+        case 'U' - 'E': return wert/USD_EUR;
+        case 'U' - 'D': return wert*USD_DM;
+        case 'E' - 'D': return wert*EUR_DM;
+        case 'D' - 'U': return wert/USD_DM;
+        case 'D' - 'E': return wert/EUR_DM;
 
-        case 'U' - 'E': resultat = wert/USD_EUR; return 1.1;
         default: return 2.2;
 
     }
+}
 
+void check (char wahl) {
+switch (wahl) {
+    case 'U':
+    case 'E':
+    case 'D': return;
+    default: printf("Fehlerhafte Eingabe!"); exit (1);
+}
 
-    return resultat;
 }
