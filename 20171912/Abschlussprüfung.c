68959fb5cd4973f950aabaf9f7cd40de4e97f029
vb4 <33378779+vb4@users.noreply.github.com>
2017-12-19 08:13:16 +0100

    Funktionsprototypen

--- 20171912/Abschlussprüfung.c
+++ 20171912/Abschlussprüfung.c
@@ -1,8 +1,23 @@
+#include "stdio.h"
 
+#define TRUE 1
 
+double BerechneEndwert(double, double);
+double AusgabeNoteA(double);
+double AusgabeNoteB(double);
+
+int bestanden;
 
 int main() {
+double noteA, noteB;
+
+printf("Note Teil A:");
+scanf("%fl", &noteA);
+printf("Note Teil B:");
+scanf("%fl", &noteB);
+bestanden = TRUE;
+printf("Bestanden: %fl", BerechneEndwert(noteA, noteB));
 
 
-    
 }
+
